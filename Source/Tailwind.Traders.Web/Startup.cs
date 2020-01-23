47e2386851c072db20e6a04ba976e31acc1aede0
CarlosM2099 <54601783+CarlosM2099@users.noreply.github.com>
2020-01-23 12:43:14 -0800

    Update Startup.cs

--- Source/Tailwind.Traders.Web/Startup.cs
+++ Source/Tailwind.Traders.Web/Startup.cs
@@ -1,9 +1,11 @@
-using Microsoft.AspNetCore.Builder;
+
+	using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Tailwind.Traders.Web.Standalone;
 
@@ -24,7 +26,7 @@ namespace Tailwind.Traders.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddOptions();
             services.Configure<Settings>(Configuration);
             // In production, the React files will be served from this directory
@@ -36,7 +38,8 @@ namespace Tailwind.Traders.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -53,20 +56,22 @@ namespace Tailwind.Traders.Web
                 ServeUnknownFileTypes = true
             });
 
-            app.UseStandalone(Configuration, Logger);
+            app.UseHttpsRedirection();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
-                });
+                endpoints.MapControllers();
+                //endpoints.MapControllerRoute("default", "{controller}/{action=Index}/{id?}");
+            });
 
             app.UseSpa(spa =>
             {
                 spa.Options.SourcePath = "ClientApp";
-  
-
                 if (env.IsDevelopment())
                 {
                     spa.UseReactDevelopmentServer(npmScript: "start");
